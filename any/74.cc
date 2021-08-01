df48d227ec41ee005592dcad68a657bcb060c044
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-08-01 19:59:48 +0530

    search in sorted 2d matrix

---
+++ any/74.cc
@@ -0,0 +1,108 @@
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+template <typename T>
+concept is_container = requires(T a) {
+  a.begin();
+  a.end();
+};
+
+template <is_container T>
+requires(!std::same_as<T, std::string>) std::ostream &
+operator<<(std::ostream &os, const T &cont) {
+  os << '{';
+  for (const auto &x : cont) {
+    os << x;
+    os << ' ';
+  }
+  os << '}';
+  return os;
+}
+
+void printMatrix(const is_container auto &cont) {
+  for (const auto &x : cont) {
+    cout << x;
+    cout << '\n';
+  }
+}
+
+template <typename T, std::size_t... Is>
+constexpr std::array<T, sizeof...(Is)>
+create_array(T value, std::index_sequence<Is...>) {
+  return {{(static_cast<void>(Is), value)...}};
+}
+
+template <std::size_t N, typename T>
+constexpr std::array<T, N> create_array(const T &value) {
+  return create_array(value, std::make_index_sequence<N>());
+}
+template <typename T> constexpr auto accessor(T &t) {
+  return [&](int i) -> typename T::reference { return t.at(i); };
+}
+
+template <typename T> constexpr auto accessor(const T &t) {
+  return [&](int i) { return t.at(i); };
+}
+
+template <typename T> constexpr auto const_accessor(T &t) {
+  return [&](int i) { return t.at(i); };
+}
+
+template <typename T> constexpr auto matrix_accessor(T &t) {
+  return [&](int i, int j) ->
+         typename T::value_type::reference { return t.at(i).at(j); };
+}
+
+template <typename T> constexpr auto matrix_accessor(const T &t) {
+  return [&](int i, int j) { return t.at(i).at(j); };
+}
+
+template <typename T> constexpr auto const_matrix_accessor(T &t) {
+  return [&](int i, int j) { return t.at(i).at(j); };
+}
+
+template <typename T> using lmt = std::numeric_limits<T>;
+
+template <typename T, std::size_t N>
+constexpr std::size_t array_size(const T (&)[N]) noexcept {
+  return N;
+}
+
+bool searchMatrix(vector<vector<int>> &matrix, int target) {
+  const auto m = matrix.size();
+  const auto n = matrix[0].size();
+  auto matrix_index = [&](size_t i) -> pair<size_t, size_t> {
+    return {i / n, i % n};
+  };
+  auto matrix_val = [&](size_t i) {
+    const auto [x, y] = matrix_index(i);
+    return matrix[x][y];
+  };
+  int low = 0;
+  int high = m * n - 1;
+  while (low <= high) {
+    const int mid = (low + high) / 2;
+    if (matrix_val(mid) == target) {
+      return true;
+    } else if (matrix_val(mid) > target) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return false;
+}
+
+int main(int argc, const char **argv) {
+  vector<vector<int>> matrix = {
+      {1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};
+  cout << searchMatrix(matrix, 3) << endl;
+  return 0;
+}
