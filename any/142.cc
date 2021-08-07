cc946d91359613cd1b2a31d529681c93199c36b1
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-08-07 17:48:53 +0530

    added detect cycle in list 2

---
+++ any/142.cc
@@ -0,0 +1,116 @@
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+template <typename T>
+concept is_container = requires(T a) {
+  a.begin();
+  a.end();
+};
+
+template <is_container T>
+requires(!std::same_as<T, std::string>) std::ostream &
+operator<<(std::ostream &os, const T &cont) {
+  os << '{';
+  for (const auto &x : cont) {
+    os << x;
+    os << ' ';
+  }
+  os << '}';
+  return os;
+}
+
+void printMatrix(const is_container auto &cont) {
+  for (const auto &x : cont) {
+    cout << x;
+    cout << '\n';
+  }
+}
+
+template <typename T, std::size_t... Is>
+constexpr std::array<T, sizeof...(Is)>
+create_array(T value, std::index_sequence<Is...>) {
+  return {{(static_cast<void>(Is), value)...}};
+}
+
+template <std::size_t N, typename T>
+constexpr std::array<T, N> create_array(const T &value) {
+  return create_array(value, std::make_index_sequence<N>());
+}
+template <typename T> constexpr auto accessor(T &t) {
+  return [&](int i) -> typename T::reference { return t.at(i); };
+}
+
+template <typename T> constexpr auto accessor(const T &t) {
+  return [&](int i) { return t.at(i); };
+}
+
+template <typename T> constexpr auto const_accessor(T &t) {
+  return [&](int i) { return t.at(i); };
+}
+
+template <typename T> constexpr auto matrix_accessor(T &t) {
+  return [&](int i, int j) ->
+         typename T::value_type::reference { return t.at(i).at(j); };
+}
+
+template <typename T> constexpr auto matrix_accessor(const T &t) {
+  return [&](int i, int j) { return t.at(i).at(j); };
+}
+
+template <typename T> constexpr auto const_matrix_accessor(T &t) {
+  return [&](int i, int j) { return t.at(i).at(j); };
+}
+
+template <typename T> using lmt = std::numeric_limits<T>;
+
+template <typename T, std::size_t N>
+constexpr std::size_t array_size(const T (&)[N]) noexcept {
+  return N;
+}
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+  ListNode *detectCycle(ListNode *head) {
+    if(head == nullptr) return head;
+    auto slow = head;
+    auto fast = head;
+    bool has_cycle = false;
+    while(fast and fast->next){
+      slow = slow->next;
+      fast = fast->next->next;
+      if(slow == fast) {
+        has_cycle = true;
+        break;
+      }
+    }
+    if(not has_cycle) return nullptr;
+    slow = head;
+    while(slow != fast){
+      slow = slow->next;
+      fast = fast->next;
+    }
+    return slow;
+  }
+};
+
+int main(){
+  vector<ListNode> vec = {0, 1, 2};
+  for(int i = 0; i < size(vec); i++){
+    vec[i].next = &vec[(i + 1) % size(vec)];
+  }
+  Solution s;
+  s.detectCycle(&vec[0]);
+}
