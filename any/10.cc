38b34644d5817a71f9ac6cca7a707dc5566164f1
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-11-01 14:25:17 +0530

    added some more things

--- any/10.cc
+++ any/10.cc
@@ -10,6 +10,7 @@
 
 using namespace std;
 
+
 class Solution {
 public:
   bool isMatch(string s, string p) {
@@ -17,18 +18,17 @@ public:
     const int n = size(p);
     vector dp(m + 1, vector(n + 1, false));
     dp[0][0] = true;
-    for (int i = 2; i <= n; i++) {
-      dp[0][i] = p[i - 1] == '*' and dp[0][i - 2];
+    for (int j = 2; j <= n; j++) {
+      if (p[j - 1] == '*') dp[0][j] = dp[0][j - 2];
     }
     for (int i = 1; i <= m; i++) {
       for (int j = 1; j <= n; j++) {
-        if (s[i - 1] == p[j - 1] or p[j - 1] == '.') {
+        if (s[i - 1] == p[j - 1] or p[j - 1] == '.')
           dp[i][j] = dp[i - 1][j - 1];
-        } else if (p[j - 1] == '*') {
+        else if (p[j - 1] == '*')
           dp[i][j] =
             dp[i][j - 2]
-            or (s[i - 1] == p[j - 2] or p[j - 2] == '.') and dp[i - 1][j];
-        }
+            || ((p[j - 2] == s[i - 1] || p[j - 2] == '.') && dp[i - 1][j]);
       }
     }
     return dp[m][n];
