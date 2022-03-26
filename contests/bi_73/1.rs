67af1d14485c37bc26bcea75e0762c2d58b9e418
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2022-03-27 01:22:15 +0530

    first ever rust code

--- contests/bi_73/1.rs
+++ contests/bi_73/1.rs
@@ -1,7 +1,18 @@
+use std::collections::HashMap;
+
 impl Solution {
     pub fn most_frequent(nums: Vec<i32>, key: i32) -> i32 {
-        nums.iter()
-            .slice(2)
-            .filter(|&v| -> first(&v) == key)
+        let mut freq_cnt = HashMap::new();
+        let iter = nums
+            .windows(2)
+            .filter(|&window| window[0] == key)
+            .map(|window| window[1]);
+        for ele in iter {
+            *freq_cnt.entry(ele).or_insert(0) += 1;
+        }
+        match freq_cnt.iter().max_by(|a, b| a.1.cmp(&b.1)) {
+            Some(p) => *p.0,
+            None => 0,
+        }
     }
 }
